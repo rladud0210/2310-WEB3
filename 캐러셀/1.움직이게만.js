273f5ffddf8b3e3009c6ea3656ab7ec3e5ab0eb4
rladud0210 <144298217+rladud0210@users.noreply.github.com>
2023-10-30 12:54:08 +0900

    .

--- 캐러셀/1.움직이게만.js
+++ 캐러셀/1.움직이게만.js
@@ -1,38 +1,32 @@
 const contentsWrapper = 
 document.querySelector('.contents-wrapper');
 
-//contentsWrapper.style.transform = 
-//`translateX(-300px)`
+// contentsWrapper.style.transform = 
+// `translateX(-300px)`
+
 
 const nextButton =
 document.querySelector('.next');
-
 const prevButton =
 document.querySelector('.prev');
 
 
-// nextButton을 누를 때 -300
+// nextButton을 누를떄 -300
+
 
 let 슬라이더위치 = 0;
 
 nextButton.addEventListener('click', ()=>{
     슬라이더위치 = 슬라이더위치 - 300
     위치적용();
-    //contentsWrapper.style.transform = 
-    //`translateX(${슬라이더위치}px)`
 })
 
 prevButton.addEventListener('click', ()=>{
     슬라이더위치 = 슬라이더위치 + 300
     위치적용();
-    //contentsWrapper.style.transform = 
-    //`translateX(${슬라이더위치}px)`
 })
 
-
-// 자주쓰는 것 function으로 묶어서 사용
 function 위치적용() {
     contentsWrapper.style.transform = 
     `translateX(${슬라이더위치}px)`
 }
-
--- 캐러셀/2.인덱스활용.js
+++ 캐러셀/2.인덱스활용.js
@@ -2,69 +2,48 @@ const contentsWrapper = document.querySelector('.contents-wrapper');
 const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 let index = 0;
-//childElementCount 에는 html 요소가 가진 자식의 갯수가 들어있다.
+// childeElementCount 에는 html요소가 가진 자식의 갯수가 들어있다
 const 슬라이드갯수 = contentsWrapper.childElementCount
 console.log(슬라이드갯수)
-
-// prev 버튼을 누르면 인덱스가 1 감소하게
-// next 버튼을 누르면 인덱스가 1 증가하게
-
+// prev버튼을 누르면 인덱스가 1 감소하게
 prevButton.addEventListener('click', ()=>{
+    // if문을 이용해 버튼을 누를때 범위 밖으로 벗어나려고 할 떄
+    // 범위 밖으로 넘어가지 않도록 작성해주세요.
+    
+    // index = index - 1
     index--;
     if(index < 0){
         index = 슬라이드갯수-1;
     }
-    
-    
-    위치적용(); 
-})
 
-nextButton.addEventListener('click', ()=>{
+    위치적용();
+})
+// next버튼을 누르면 인덱스가 1 증가하게
+nextButton.addEventListener('click',()=>{
     index++;
-    if(index > 슬라이드갯수-1){
-        index = 0
+    if(index > 슬라이드갯수-1) {
+        index = 0;
     }
-    // 배열은 0부터 시작. 2번 다음으로 못 넘어가게
-    
-    위치적용();   
+    위치적용();
 })
-
 
 function 위치적용() {
     contentsWrapper.style.transform = 
     `translateX(${index * -300}px)`
     // index 0 : 값 0
     // index 1 : 값 -300
-    // ...
+    // index 2 : 값 -600
+    // 위 규칙을 식으로 나타내면 index * -300이다.
 
-    // index * -300
-    
     nowPage.innerText = index+1;
 }
 
-
-
-
-
-
-// 나우페이지 가져오기
-// 토탈 페이지 가져오기
-
-// 인덱스에 따라 나우페이지가 변하게끔
-// 토탈페이지는 전체 슬라이드 갯수로 설정해주세요
-
-// 나우페이지는 prev, next 버튼 누를 때마다 갱신되어야 합니다.
-
-const nowPage = document.querySelector('.now-page');
+const nowPage = document.querySelector('.now-page')
 const totalPage = document.querySelector('.total-page');
 
 totalPage.innerText = 슬라이드갯수;
 
+// 인덱스에 따라 나우페이지가 변하게끔
+// 토탈페이지는 전체 슬라이드 갯수로 설정해주세요.
 
-const 페이지 = pageContainer.childElementCount
-console.log(페이지)
-
-
-
-
-
+// 나우페이지는 prev,next 버튼을 누를때마다 갱신되어야합니다.
